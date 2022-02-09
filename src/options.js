cb62c236bc300223dd8e9cd832971fa858e47c9d
Marcus Chan <marcusgchan35@gmail.com>
2022-02-09 13:40:13 -0800

    added options

---
+++ src/options.js
@@ -0,0 +1,32 @@
+const OPTIONS = [
+  {
+    id: 1,
+    option: "name",
+  },
+  {
+    id: 2,
+    option: "animal type",
+  },
+  {
+    id: 3,
+    option: "latin name",
+  },
+  {
+    id: 4,
+    option: "habitat",
+  },
+  {
+    id: 5,
+    option: "lifespan",
+  },
+  {
+    id: 6,
+    option: "min weight",
+  },
+  {
+    id: 7,
+    option: "max weight",
+  },
+];
+
+export default OPTIONS;
